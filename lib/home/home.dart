2968d92a3ad79bc4bed6d31e797d2c3904cca831
Izer Onadim <izeronadim@gmail.com>
2022-05-01 10:42:17 +0100

    Lesson 17: set up auth stream

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,26 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:quizapp/login/login.dart';
+import 'package:quizapp/services/auth.dart';
+import 'package:quizapp/topics/topics.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              child: Text('about', style: Theme.of(context).textTheme.button),
-              onPressed: () => Navigator.pushNamed(context, '/about'),
-            ),
-            ElevatedButton(
-              child: Text('topics', style: Theme.of(context).textTheme.button),
-              onPressed: () => Navigator.pushNamed(context, '/topics'),
-            ),
-          ],
-        ),
-      ),
+    return StreamBuilder(
+      stream: AuthService().userStream,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text('Loading');
+        } else if (snapshot.hasError) {
+          return const Center(child: Text('Error'));
+        } else if (snapshot.hasData) {
+          return const TopicsScreen();
+        } else {
+          return const LoginScreen();
+        }
+      },
     );
   }
 }
