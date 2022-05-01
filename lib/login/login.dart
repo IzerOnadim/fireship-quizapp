7838b480af662d92446d62f3bc90883bc2a384f9
Izer Onadim <izeronadim@gmail.com>
2022-05-01 11:51:10 +0100

    Lesson 18: Add login screen

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:quizapp/services/auth.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -7,6 +9,61 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Login")),
+      body: Container(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const FlutterLogo(
+              size: 150,
+            ),
+            Flexible(
+              child: LoginButton(
+                colour: Colors.deepPurple,
+                icon: FontAwesomeIcons.userNinja,
+                text: 'Continue as a Guest',
+                loginMethod: AuthService().anonLogin,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginButton extends StatelessWidget {
+  final Color colour;
+  final IconData icon;
+  final String text;
+  final Function loginMethod;
+
+  const LoginButton({
+    Key? key,
+    required this.colour,
+    required this.icon,
+    required this.text,
+    required this.loginMethod,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10),
+      child: ElevatedButton.icon(
+        onPressed: () => loginMethod(),
+        icon: Icon(
+          icon,
+          color: Colors.white,
+          size: 20,
+        ),
+        style: TextButton.styleFrom(
+          padding: const EdgeInsets.all(24),
+          backgroundColor: colour,
+        ),
+        label: Text(text),
+      ),
     );
   }
 }
